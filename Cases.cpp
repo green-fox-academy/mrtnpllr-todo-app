bedbb728d22649bf37e230990d850969708b6eb5
Marton Piller <mrtn.pllr@gmail.com>
2019-07-24 17:09:33 +0200

    Init cases functions

---
+++ Cases.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <iostream>
+#include "Cases.h"
+#include "Functions.h"
+
+int useTwoArguments (char *argv[])
+{
+
+}
---
+++ Cases.h
@@ -0,0 +1,8 @@
+#ifndef MRTNPLLR_TODO_APP_CASES_H
+#define MRTNPLLR_TODO_APP_CASES_H
+
+
+int useTwoArguments (char *argv[]);
+
+
+#endif
